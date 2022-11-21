da63b071a22b4226862721764bf5beccba548c3e
stanislavKosacek <stanislav.kosacek@icloud.com>
2022-11-21 22:46:52 +0100

    oprava názvu tabulky

--- src/functions/db.js
+++ src/functions/db.js
@@ -3,12 +3,12 @@ import { getSupabase } from './supabase.js';
 export const getShopingItems = (userId) => {
   const supabase = getSupabase();
 
-  return supabase.from('shoping_item').select('*').eq('user_id', userId);
+  return supabase.from('shopping_item').select('*').eq('user_id', userId);
 };
 
 export const addShoppingItem = (product, amount, unit, userId) => {
   const supabase = getSupabase();
-  return supabase.from('shoping_item').insert({
+  return supabase.from('shopping_item').insert({
     product: product,
     amount: amount,
     unit: unit,
